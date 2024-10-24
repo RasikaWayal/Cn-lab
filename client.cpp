91f3881811bbf0d6d3a6f6a86dfc475c8d8259f9
RasikaWayal <rasikawayal15@gmail.com>
2024-10-24 11:43:33 +0530

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -1,53 +1,43 @@
 #include<iostream>
-#include<fstream>
-#include<stdlib.h>
-#include<netinet/in.h>
 #include<unistd.h>
+#include<netinet/in.h>
 #include<string.h>
-#include<sys/socket.h>
-#include<stdio.h>
 #include<string>
+#include<fstream>
+#include <arpa/inet.h>
+#include<sys/socket.h>
 
-#define PORT 7512
+#define PORT 6511
 #define MAXLINES 1024
-
 using namespace std;
-
 int main()
 {
+
     int sockfd;
-    string fileName;
+    sockaddr_in server;
     char buffer[MAXLINES];
-    struct sockaddr_in server;
+    string fileName;
 
-    sockfd = socket(AF_INET,SOCK_DGRAM,0);
-    memset(&server,0,sizeof(server));
-
+    if( (sockfd = socket(AF_INET,SOCK_STREAM,0) ) < 0 )
+    {
+        cout << "err";
+    }
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    cout << "Enter file name" << endl;
+    if(connect(sockfd,(const sockaddr *) &server,sizeof(server)) < 0 )
+        cout << "error";
+
+    cout << "Enter fileName" << endl;
     cin >> fileName;
+    send(sockfd,fileName.c_str(),fileName.length(),0);
 
-    cout << fileName.c_str() << endl;
-    sendto(sockfd, (const char *)fileName.c_str(), fileName.length(),0,(const struct sockaddr *)&server, sizeof(server));
-    cout << "Sent filename to server" << endl;
+    int n = read(sockfd,buffer,MAXLINES);
 
-    cout << "Waiting for response" << endl;
-    socklen_t len;
-    int n = recvfrom(sockfd,(char *)buffer,sizeof(buffer),0,(struct sockaddr *)&server,&len);
-    buffer[n] = '\0';
-    cout << "Response recieved: " << buffer << endl;
-    
-    ofstream ofs;
-    cout << "Creating file" << endl;
-    ofs.open(fileName,ios::out);
-    cout << "Writing to file" << endl;
+    ofstream ofs(fileName,ios::out);
     ofs.write(buffer,n);
-    cout << "Closing file" << endl;
     ofs.close();
-    cout << "Closing socket"<<endl;
-    close(sockfd);
+
     return 0;
 }
